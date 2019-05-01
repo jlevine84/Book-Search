3484c6e9c68752053874c8a7b735d17bc6458f00
Jeff Levine <jlevine_84@yahoo.com>
2019-05-01 19:14:26 -0400

    BookSearch Component

--- client/src/components/BookSearch/BookSearch.js
+++ client/src/components/BookSearch/BookSearch.js
@@ -3,8 +3,18 @@ import './booksearch.css'
 
 function BookSearch() {
   return (
-    <div>
-      BookSearch
+    <div className="container booksearch">
+      <form>
+        <div className="form-group searchinput">
+          <input 
+          type="text"
+          className="form-control"
+          name="searchInput"
+          placeholder="Search for a Book!"
+          />
+        </div>
+        <button type="submit" class="btn btn-success searchbtn">Search</button>
+      </form>
     </div>
   )
 }
