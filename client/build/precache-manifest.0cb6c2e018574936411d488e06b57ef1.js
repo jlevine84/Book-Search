a0a445bb5d3ce020819e3776ef1c959f0b364da1
Jeff Levine <jlevine_84@yahoo.com>
2019-05-03 18:28:17 -0400

    run build

---
+++ client/build/precache-manifest.0cb6c2e018574936411d488e06b57ef1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6252bba87abd3f72828",
+    "url": "/static/css/main.0fde740c.chunk.css"
+  },
+  {
+    "revision": "a6252bba87abd3f72828",
+    "url": "/static/js/main.4e1cd076.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "980b614f999c0f956ec6",
+    "url": "/static/js/2.ade8b1dc.chunk.js"
+  },
+  {
+    "revision": "14b223c0c8a36126e7fef20d4333f1a2",
+    "url": "/static/media/booktitle.14b223c0.jpg"
+  },
+  {
+    "revision": "ddee0476d5236053ed0a961db6ebbfb4",
+    "url": "/index.html"
+  }
+];
