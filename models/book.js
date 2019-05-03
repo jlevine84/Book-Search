a7c0563df127e16e693023484f9e866b2eb9dbd6
Jeff Levine <jlevine_84@yahoo.com>
2019-05-03 14:56:12 -0400

    model update

--- models/book.js
+++ models/book.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
+  title: { type: String, required: true, unique: true },
+  author: { type: String, required: true, unique: true },
   synopsis: String,
-  image: String,
+  image: { type: String, unique: true },
   date: { type: Date, default: Date.now }
 });
 
